2659a4f342b916a5f1c4695252caaa305dcef0d4
changxicao <941038274@qq.com>
2015-06-08 11:37:56 +0800

    上传工程代码

---
+++ tenMile/tenMile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tenMile
+//
+//  Created by changxicao on 15/6/8.
+//  Copyright (c) 2015年 changxicao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
