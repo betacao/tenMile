38bb90c010d98d75ec54e18dd9f7cd551b01ff9c
changxicao <941038274@qq.com>
2015-06-10 21:11:39 +0800

    增加新的界面

--- tenMile/tenMile/ObjTabBarViewController.m
+++ tenMile/tenMile/ObjTabBarViewController.m
@@ -9,11 +9,16 @@
 #import "ObjTabBarViewController.h"
 #import "ObjHomeViewController.h"
 #import "ObjNavigationViewController.h"
+#import "ObjUserSettingViewController.h"
+#import "ObjStoresViewController.h"
+#import "ObjCartViewController.h"
+
 
 @interface ObjTabBarViewController ()<UITabBarDelegate>
 
-@property (strong, nonatomic)   ObjHomeViewController *homeViewController;
 @property (weak, nonatomic)     IBOutlet UITabBar *tabBarView;
+@property (strong, nonatomic)   NSArray *viewControllers;
+@property (assign, nonatomic)   NSInteger currentIndex;
 
 
 @end
@@ -22,10 +27,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.homeViewController = [[ObjHomeViewController alloc] initWithNibName:@"ObjHomeViewController" bundle:nil];
-    ObjNavigationViewController *nav = [[ObjNavigationViewController alloc] initWithRootViewController:self.homeViewController];
-    [self addChildViewController:nav];
-    [self.view insertSubview:nav.view belowSubview:self.tabBarView];
+    //首页
+    ObjHomeViewController *homeViewController = [[ObjHomeViewController alloc] initWithNibName:@"ObjHomeViewController" bundle:nil];
+    ObjNavigationViewController *nav1 = [[ObjNavigationViewController alloc] initWithRootViewController:homeViewController];
+    [self addChildViewController:nav1];
+    [self.view insertSubview:nav1.view belowSubview:self.tabBarView];
+    
+    //商城
+    ObjStoresViewController *storesViewController = [[ObjStoresViewController alloc] initWithNibName:@"ObjStoresViewController" bundle:nil];
+    ObjNavigationViewController *nav2 = [[ObjNavigationViewController alloc] initWithRootViewController:storesViewController];
+    
+    //购物车
+    ObjCartViewController *cartViewController = [[ObjCartViewController alloc] initWithNibName:@"ObjCartViewController" bundle:nil];
+    ObjNavigationViewController *nav3 = [[ObjNavigationViewController alloc] initWithRootViewController:cartViewController];
+    
+    //个人中心
+    ObjUserSettingViewController *settingViewController = [[ObjUserSettingViewController alloc] initWithNibName:@"ObjUserSettingViewController" bundle:nil];
+    ObjNavigationViewController *nav4 = [[ObjNavigationViewController alloc] initWithRootViewController:settingViewController];
+    
+    self.viewControllers = @[nav1,nav2,nav3,nav4];
+    
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -44,10 +65,20 @@
 - (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item
 {
     NSInteger index = [tabBar.items indexOfObject:item];
+    if(self.currentIndex == index) {
+        return;
+    }
     switch (index) {
         case 0:
         {
             //首页
+            ObjNavigationViewController *fromController = [self.viewControllers objectAtIndex:self.currentIndex];
+            ObjNavigationViewController *toController = [self.viewControllers objectAtIndex:index];
+            [fromController.view removeFromSuperview];
+            [fromController removeFromParentViewController];
+            
+            [self addChildViewController:toController];
+            [self.view insertSubview:toController.view belowSubview:self.tabBarView];
         }
             break;
         case 1:
@@ -63,12 +94,20 @@
         case 3:
         {
             //个人信息
+            ObjNavigationViewController *fromController = [self.viewControllers objectAtIndex:self.currentIndex];
+            ObjNavigationViewController *toController = [self.viewControllers objectAtIndex:index];
+            [fromController.view removeFromSuperview];
+            [fromController removeFromParentViewController];
+            
+            [self addChildViewController:toController];
+            [self.view insertSubview:toController.view belowSubview:self.tabBarView];
         }
             break;
             
         default:
             break;
     }
+    self.currentIndex = index;
 }
 
 - (UIStatusBarStyle)preferredStatusBarStyle
---
+++ tenMile/tenMile/Views/storeViews/ObjStoresViewController.h
@@ -0,0 +1,13 @@
+//
+//  ObjStoresViewController.h
+//  tenMile
+//
+//  Created by changxicao on 15/6/10.
+//  Copyright (c) 2015年 changxicao. All rights reserved.
+//
+
+#import "ObjViewController.h"
+
+@interface ObjStoresViewController : ObjViewController
+
+@end
